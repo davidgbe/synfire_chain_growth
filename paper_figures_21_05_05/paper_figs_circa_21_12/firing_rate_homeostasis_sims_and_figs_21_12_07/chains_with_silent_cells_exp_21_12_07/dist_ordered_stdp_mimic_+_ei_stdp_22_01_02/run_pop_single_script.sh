efbce49f09abd2f57653f4b2235fa1e40db56eea
David Bell <davidgrebell@gmail.com>
2022-01-03 18:01:57 -0800

    try settling some new chains with revised networks

---
+++ paper_figures_21_05_05/paper_figs_circa_21_12/firing_rate_homeostasis_sims_and_figs_21_12_07/chains_with_silent_cells_exp_21_12_07/dist_ordered_stdp_mimic_+_ei_stdp_22_01_02/run_pop_single_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+caffeinate python3 run.py --title ei_stdp_10ms --alpha_1 6e-2 --alpha_2 0.5e-2 --beta 1e-2 --gamma 0 --fr_single_line_attr 1 --rng_seed $1 --dropout_per 0.5 --synfire_prop_dist 1. --drop_iter $2
