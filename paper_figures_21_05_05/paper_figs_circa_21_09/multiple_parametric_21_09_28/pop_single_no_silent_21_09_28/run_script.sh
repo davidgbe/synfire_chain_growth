db82919206d922202f6c7b089d0c6b852015408a
davidgbe <davidgrebell@gmail.com>
2022-01-05 15:19:31 -0800

    commit newest nb changes

--- paper_figures_21_05_05/paper_figs_circa_21_09/multiple_parametric_21_09_28/pop_single_no_silent_21_09_28/run_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-caffeinate python3 run.py --title single_only --alpha_1 3e-2 --alpha_2 1.5e-2 --beta 0 --gamma 0 --fr_single_sym 1 --rng_seed $1 --dropout_per 0.5 --w_e_e_scale_down_factor 0 --w_e_i_scale_down_factor 0
