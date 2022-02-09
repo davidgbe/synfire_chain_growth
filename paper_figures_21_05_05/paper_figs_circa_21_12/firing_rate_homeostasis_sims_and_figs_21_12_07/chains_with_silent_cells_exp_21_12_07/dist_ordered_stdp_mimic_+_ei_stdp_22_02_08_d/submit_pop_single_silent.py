25466373718a38d88ab6802e8305079a8619674e
David Bell <davidgrebell@gmail.com>
2022-02-09 10:27:11 -0800

    prepare new axonal delay networks for dropout

--- paper_figures_21_05_05/paper_figs_circa_21_12/firing_rate_homeostasis_sims_and_figs_21_12_07/chains_with_silent_cells_exp_21_12_07/dist_ordered_stdp_mimic_+_ei_stdp_22_02_08_d/submit_pop_single_silent.py
+++ paper_figures_21_05_05/paper_figs_circa_21_12/firing_rate_homeostasis_sims_and_figs_21_12_07/chains_with_silent_cells_exp_21_12_07/dist_ordered_stdp_mimic_+_ei_stdp_22_02_08_d/submit_pop_single_silent.py
@@ -75,11 +75,11 @@ def pad_zeros(to_pad, length):
 batch_size = 10
 
 params = OrderedDict()
-params['SEED'] = [str(i) for i in range(2090, 2110)]
-params['ALPHA_1'] = [ str(3e-2) ]
-params['BETA'] = [ str(1e-3) ]
-params['GAMMA'] = [ str(1e-1) ]
-params['SYN_PROP_DIST'] = [ str(1.3) ]
+params['SEED'] = [str(i) for i in range(2090, 2120)]
+params['ALPHA_1'] = [ str(5e-2) ]
+params['BETA'] = [ str(2e-3) ]
+params['GAMMA'] = [ str(1e-2) ]
+params['SYN_PROP_DIST'] = [ str(1.) ]
 params['DROP_SEV'] = [str(0.5)]
 
 for key in params.keys():
