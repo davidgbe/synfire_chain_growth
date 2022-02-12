7c86f78b1a6a365eece357a9f617292a878402e5
David Bell <davidgrebell@gmail.com>
2022-02-12 13:29:21 -0800

    with another folder for hopefully paper-ready sims

---
+++ paper_figures_21_05_05/paper_figs_circa_21_12/paper_sims_and_figs_22_02_12/chains_with_silent_cells_22_02_12/parabolic_integrated_22_02_12/run_pop_single_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+caffeinate python3 run.py --title stdp --alpha 5e-2 --beta 5e-3 --beta_2 0 --gamma 1e-2 --fr_single_line_attr 1 --rng_seed $1 --dropout_per 0.5 --synfire_prop_dist 12 --drop_iter $2
